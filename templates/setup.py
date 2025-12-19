da6be3094943b5186f0d7b327c9b746dd6c3746b
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2025-12-19 09:29:57 -0800

    Add '/opt/tcl-tk' to library_dirs in setup.py

--- templates/setup.py
+++ templates/setup.py
@@ -16,6 +16,6 @@ setup(name="tkinter",
                   define_macros=[("WITH_APPINIT", 1), ("TCL_WITH_EXTERNAL_TOMMATH", 1)],
                   include_dirs=["@INCLUDE@/internal", "@INCLUDE@/tcl", "@INCLUDE@/tk"],
                   libraries=["tcl8.6"],
-                  library_dirs=["/usr", "/usr/local"])
+                  library_dirs=["/usr", "/usr/local", "/opt/tcl-tk"])
       ]
 )
